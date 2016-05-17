ffaef7ffa03c4d35b53f9ac393d215ee58ab0209
Paula Dutra <paulavdutr@gmail.com>
2016-05-17 15:55:49 -0300

    arquivo adicionado

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma palavra para iniciar o jogo:");
+            string palavra = Console.ReadLine();
+            int aux = 0;
+            char[] ast = new char [palavra.Length];
+
+            while(aux < 7)
+            {
+                Console.WriteLine("Digite uma letra:");
+                char letra = char.Parse(Console.ReadLine());
+                
+
+                for (int i = 0; i < palavra.Length; i++)
+			    {
+                    if(letra == palavra[i])
+                    {
+                        ast[i] = letra;                                                                     
+                    }
+                    else
+                    {
+                        ast[i] = '*';
+                        aux ++;
+                    }                                     
+                }
+
+               Console.WriteLine(ast);
+            }
+        }
+    }
+}
