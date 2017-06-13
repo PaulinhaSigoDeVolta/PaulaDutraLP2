eb715e7f4a08ba9b6b35ff4eca4f7d4dd7915b1b
PaulinhaSigoDeVolta <paulavdutr@gmail.com>
2017-06-13 14:23:14 -0300

    Adicio

---
+++ ExercicioFormulario/ExercicioFormulario/Alterar.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace ExercicioFormulario
+{
+    /// <summary>
+    /// Interaction logic for Alterar.xaml
+    /// </summary>
+    public partial class Alterar : Window
+    {
+        public Alterar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAlterar1_Click(object sender, RoutedEventArgs e)
+        {
+            MySqlCommand cmd = new MySqlCommand()
+            {
+                Connection = new MySqlConnection("Server=LocalHost;DataBase=cadastro;Uid=root;Pwd="),
+                CommandText = "UPDATE pessoa SET Salario = @salario WHERE Matricula = @matricula"
+            };
+
+            cmd.Parameters.AddWithValue("@Matricula", txtMatricula1.Text);
+            cmd.Parameters.AddWithValue("@Salario", txtSalario1.Text);
+
+            cmd.Connection.Open();
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+
+            MessageBox.Show("Alterado !");
+        }
+    }
+}
---
+++ ExercicioFormulario/ExercicioFormulario/Excluir.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace ExercicioFormulario
+{
+    /// <summary>
+    /// Interaction logic for Excluir.xaml
+    /// </summary>
+    public partial class Excluir : Window
+    {
+        public Excluir()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MySqlCommand cmd = new MySqlCommand()
+            {
+                Connection = new MySqlConnection("Server=LocalHost;DataBase=cadastro;Uid=root;Pwd="),
+                CommandText = "DELETE FROM pessoa WHERE Matricula = @matricula"
+            };
+
+            cmd.Parameters.AddWithValue("@Matricula", txtMatricula1.Text);
+
+            cmd.Connection.Open();
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+
+            MessageBox.Show("Excluido !");
+            
+
+        }
+    }
+}
